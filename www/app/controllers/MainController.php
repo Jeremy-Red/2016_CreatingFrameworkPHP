1e06620b66574c801fc8a6f5de4429b470471edc
Jeremy-red <jeremy.red.1991@gmail.com>
2024-04-28 10:31:31 +0300

    6/31 feat: model_db part 2 complete

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -9,6 +9,11 @@ class MainController extends AppController
     {
         $model = new Main();
         $posts = $model->findAll();
+        // $post = $model->findOne('Text post', 'title');
+        // $data = $model->findBySql('SELECT id, title FROM posts ORDER BY id DESC LIMIT 2');
+        // $data = $model->findBySql("SELECT * FROM {$model->table} WHERE title LIKE ?", ['%ost%']);
+        // $data = $model->findLike('post', 'title');
+        // debug($data);
         $title = 'Page title';
         $this->set(compact('title', 'posts'));
     }
